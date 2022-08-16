2d39f20677b37196d42eeea64f20ca3f1f6e844c
Chaipzor <Chaipzor@hotmail.com>
2022-08-16 19:27:26 +0200

    first commit

---
+++ src/Detalles_Pedido.java
@@ -0,0 +1,3 @@
+public class Detalles_Pedido {
+    
+}
---
+++ src/Productos.java
@@ -0,0 +1,15 @@
+import java.sql.Time;
+
+public class Productos {
+    private int id;
+    private int id_categoria;
+    private String nombre;
+    private String descripcion;
+    private double precio;
+    private int stock;
+    private double impuesto;
+    private String imagen;
+    private Time fecha_alta;
+    private boolean baja;
+}
+
